c0ede9c7030da8b78a9ef34d235b697ed7fbe32a
Hugh Carroll <hughie@carrollonline.co.uk>
2022-08-06 11:32:03 +0100

    Fix up L4.

--- assets/js/patterns/kyu.js
+++ assets/js/patterns/kyu.js
@@ -52,19 +52,19 @@ function L3() {
     reset();
 
     let s = new Stepper()
-        .setStart(k, kImg, 2)
-        .launch(low)
-        .spin(-2)
-        .fly(centre, mid)
-        .fly(lfIsh, mid, 0, .5, 1.5)
-        .spin(-8, .5, 1.5)
-        .fly(rtIsh, mid, 0, .5, 2.5)
-        .spin(8, .5, 1.5)
-        .fly(centre, mid, 0, .5, 1.5)
-        .spin(1, .5)
-        .fly(centre, lowIsh, 0, .5, 1.5)
-        .spin(-2)
-        .land(2)
+    s.setStart(k, kImg, 2)
+    s.launch(s.low)
+    s.spin(-2)
+    s.fly(centre, s.mid)
+    s.fly(s.lf1, s.mid, 0, .5, 1.5)
+    s.spin(-8, .5, 1.5)
+    s.fly(s.rt1, s.mid, 0, .5, 2.5)
+    s.spin(8, .5, 1.5)
+    s.fly(centre, s.mid, 0, .5, 1.5)
+    s.spin(1, .5)
+    s.fly(centre, s.lowIsh, 0, .5, 1.5)
+    s.spin(-2)
+    s.land(2)
 
     s.doTimeline();
 }
@@ -72,57 +72,60 @@ function L3() {
 function L4() {
     reset();
 
+    let s = new Stepper()
+    s.setStart(k, kImg, -2)
+
     let mp1 = { 
         path: [
-            {x: -262, y: -25},
-            {x: -310, y: mid},
+            {x: s.centre, y: s.low},
+            {x: s.lf1, y: s.low},
+            {x: s.lf2, y: s.lowIsh},
+            {x: s.lf2, y: s.mid},
         ],
         autoRotation: false,
         start: 0,
         end: 1,
-        curviness: .3
+        curviness: .6
     }
 
     let mp2 = {
         path: [
-            {x: lf, y: mid},
-            {x: lf, y: low},
-            {x: rt, y: low},
-            {x: rt, y: mid},
-            {x: rtIsh, y: hi},
-            {x: 0, y: hi},
+            {x: s.lf2, y: s.mid},
+            {x: s.lf2, y: s.low},
+            {x: s.rt2, y: s.low},
+            {x: s.rt2, y: s.mid},
+            {x: s.rt1, y: s.hi},
+            {x: s.centre, y: s.hi},
         ],
         start: 0,
         end: 1,
         curviness: .7,
     };
 
-    let s = new Stepper()
-        .setStart(k, kImg, -2)
-        .launch(low)
-        .path(mp1, 4)
-        .innerSpin(1, 1.8, 1, '<')
-        .fly(lfIsh, mid, 0, .5, 2)
-        .innerSpin(4, .5, .8)
-        .innerSpin(-4, .5, .8)
-        .innerSpin(-2, .5, .4)
-        .fly(rtIsh, mid)
-        .innerSpin(4, .5, .8)
-        .innerSpin(-4, .5, .8)
-        .fly(centre, mid)
-        .fly(centre, ground - 10, 0, .5, 2)
-        .fly(centre, mid, 0, .5, 2)
-        .fly(centre - 30, mid, -2, 0)
-        .fly(centre - 30, ground - 10, 0, 0, 2)
-        .fly(centre - 30, mid, 0, .5, 2)
-        .fly(lf, mid, 0, .5, 1.5)
-        .path(mp2, 8)
-        .innerSpin(-1, 0, 1, '<+1')
-        .innerSpin(-3, 0, 3, '<+2.6')
-        .spin(-1)
-        .diveStop(low)
-        .innerSpin(2, .5)
-        .land()
+    s.launch(s.low)
+    s.path(mp1, 4)
+    s.innerSpin(1, 1.6, .7, '<')
+    s.fly(s.lf1, s.mid, 0, .5, 2)
+    s.innerSpin(4, .5, .8)
+    s.innerSpin(-4, .5, .8)
+    s.innerSpin(-2, .5, .4)
+    s.fly(s.rt1, s.mid)
+    s.innerSpin(4, .5, .8)
+    s.innerSpin(-4, .5, .8)
+    s.fly(centre, s.mid)
+    s.fly(centre, s.ground - 10, 0, .5, 2)
+    s.fly(centre, s.mid, 0, .5, 2)
+    s.fly(centre - 30, s.mid, -2, 0)
+    s.fly(centre - 30, s.ground - 10, 0, 0, 2)
+    s.fly(centre - 30, s.mid, 0, .5, 2)
+    s.fly(s.lf2, s.mid, 0, .5, 1.5)
+    s.path(mp2, 8)
+    s.innerSpin(-1, 0, 1, '<+1')
+    s.innerSpin(-3, 0, 3, '<+2.6')
+    s.spin(-1)
+    s.diveStop(s.low)
+    s.innerSpin(2, .5)
+    s.land()
 
     s.doTimeline();
 }
@@ -132,17 +135,17 @@ function L5() {
 
     let figure8 = {
         path: [
-            {x: rtIsh, y: lowIsh},
-            {x: lfIsh, y: hi},
-            {x: lf, y: mid},
-            {x: lfIsh, y: low},
-            {x: rtIsh, y: hi},
-            {x: rt, y: mid},
-            {x: rtIsh, y: low},
-            {x: lfIsh, y: hi},
-            {x: lf, y: mid},
-            {x: lfIsh, y: low},
-            {x: rtIsh, y: hiIsh},
+            {x: s.rt1, y: s.lowIsh},
+            {x: s.lf1, y: s.hi},
+            {x: s.lf3, y: s.mid},
+            {x: s.lf1, y: s.low},
+            {x: s.rt1, y: s.hi},
+            {x: s.rt3, y: s.mid},
+            {x: s.rt1, y: s.low},
+            {x: s.lf1, y: s.hi},
+            {x: s.lf3, y: s.mid},
+            {x: s.lf1, y: s.low},
+            {x: s.rt1, y: s.hiIsh},
         ],
         start: 0,
         end: 1,
@@ -170,27 +173,27 @@ function L5() {
     }
 
     let s = new Stepper()
-        .setStart(k, kImg, -2)
-        .launch(low)
-        .spin(1, .3, .3)
-        .fly(lf + 40, low, 0, .5, 2)
-        .spin(-2, .3, 1.5)
-        .fly(rt, low, 0, .5, 3)
-        .spin('+=155_cw', .3, 1.5)
-        .path(figure8, 11)
-        .fly(rtIsh, hiIsh, '90_shortest', .5, .5)
-        .fly(rtIsh, lowIsh, 0, .5, 2)
-        .fly(lfIsh, lowIsh, 0, .5, 2)
-        .fly(lfIsh, hiIsh, 0, .5, 2)
-        .fly(rtIsh, hiIsh, 0, .5, 2)
-        .spin('+=60_cw', .5, 1)
-        .path(spiralIn, 6)
-        .spin('-92_ccw', .5, 1)
-        .path(spiralOut, 6)
-        .spin('-=90_ccw')
-        .diveStop(low)
-        .spin(2, .5)
-        .land()
+    s.setStart(k, kImg, -2)
+    s.launch(s.low)
+    s.spin(1, .3, .3)
+    s.fly(s.lf3 + 40, s.low, 0, .5, 2)
+    s.spin(-2, .3, 1.5)
+    s.fly(s.rt3, s.low, 0, .5, 3)
+    s.spin('+=155_cw', .3, 1.5)
+    s.path(figure8, 11)
+    s.fly(s.rt1, s.hiIsh, '90_shortest', .5, .5)
+    s.fly(s.rt1, s.lowIsh, 0, .5, 2)
+    s.fly(s.lf1, s.lowIsh, 0, .5, 2)
+    s.fly(s.lf1, s.hiIsh, 0, .5, 2)
+    s.fly(s.rt1, s.hiIsh, 0, .5, 2)
+    s.spin('+=60_cw', .5, 1)
+    s.path(spiralIn, 6)
+    s.spin('-92_ccw', .5, 1)
+    s.path(spiralOut, 6)
+    s.spin('-=90_ccw')
+    s.diveStop(s.low)
+    s.spin(2, .5)
+    s.land()
 
     s.doTimeline();
 }
@@ -254,36 +257,36 @@ function L6() {
     }
 
     let s = new Stepper()
-        .setStart(k, kImg)
-        .setStart(k, kImg, -2)
-        .launch(lowIsh)
-        .spin(3, .5, .7)
-        .spin(1, .5, .3)
-        .spin(1, .5, .3)
-        .spin(-6, .5, 1)
-        .fly(rt, lowIsh, 0, .5, 1.5)
-        .spin(2, 0, 2)
-        .fly(lf, lowIsh, 0, 0, 3)
-        .spin(-2, 0, 2)
-        .fly(centre, lowIsh, 0, 0, 1.5)
-        .fly(centre, low, 1, 0, .7)
-        .fly(centre, mid, 0, .5, 1.5)
-        .spin(-5, .5, 1)
-        .fly(rtIsh, mid)
-        .spin('+=45_cw', .5, .3)
-        .spin('+=45_cw', .5, .3)
-        .spin('+=45_cw', .5, .3)
-        .spin('+=45_cw', .5, .3)
-        .fly(lfIsh, mid)
-        .path(ironCrossNW, 2)
-        .path(ironCrossNE, 2)
-        .path(ironCrossSE, 2)
-        .path(ironCrossSW, 2)
-        .spin(1, .5)
-        .path(preDive, 3)
-        .diveStop(low, .7)
-        .spin(4, .5, .5)
-        .land()
+    s.setStart(k, kImg)
+    s.setStart(k, kImg, -2)
+    s.launch(s.lowIsh)
+    s.spin(3, .5, .7)
+    s.spin(1, .5, .3)
+    s.spin(1, .5, .3)
+    s.spin(-6, .5, 1)
+    s.fly(s.rt3, s.lowIsh, 0, .5, 1.5)
+    s.spin(2, 0, 2)
+    s.fly(s.lf3, s.lowIsh, 0, 0, 3)
+    s.spin(-2, 0, 2)
+    s.fly(centre, s.lowIsh, 0, 0, 1.5)
+    s.fly(centre, s.low, 1, 0, .7)
+    s.fly(centre, s.mid, 0, .5, 1.5)
+    s.spin(-5, .5, 1)
+    s.fly(s.rt1, s.mid)
+    s.spin('+=45_cw', .5, .3)
+    s.spin('+=45_cw', .5, .3)
+    s.spin('+=45_cw', .5, .3)
+    s.spin('+=45_cw', .5, .3)
+    s.fly(s.lf1, s.mid)
+    s.path(ironCrossNW, 2)
+    s.path(ironCrossNE, 2)
+    s.path(ironCrossSE, 2)
+    s.path(ironCrossSW, 2)
+    s.spin(1, .5)
+    s.path(preDive, 3)
+    s.diveStop(s.low, .7)
+    s.spin(4, .5, .5)
+    s.land()
 
     s.doTimeline();
 }
@@ -294,9 +297,9 @@ function L7() {
     let mp1 = {
         path: [
             {x: -135, y: -200},
-            {x: lfIsh, y: mid + 60},
-            {x: lfIsh + 40, y: low - 40},
-            {x: 0, y: low},
+            {x: s.lf1, y: s.mid + 60},
+            {x: s.lf1 + 40, y: s.low - 40},
+            {x: 0, y: s.low},
         ],
         start: 0,
         end: 1,
@@ -305,30 +308,30 @@ function L7() {
     }
 
     let s = new Stepper()
-        .setStart(k, kImg)
-        .launch(hi, 2)
-        .spin(2, .5)
-        .diveStop(low, 3)
-        .spin(4, .5, 3)
-        .spin('-=45_ccw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
-        .fly(lfIsh, mid, 0, .5, 2.5)
-        .spin('-=180_ccw', 0, .7)
-        .path(mp1, 3)
-        .innerSpin('-135_ccw', 0, 2.5, '<+=1.1')
-        .innerSpin(3, .5, 2.5)
-        .fly(rtIsh, low)
-        .innerSpin('-=360_ccw', .5, 2.5)
-        .fly(centre, low, 0, .5, 1.5)
-        .innerSpin('-=450_ccw', .5, .8)
-        .land()
+    s.setStart(k, kImg)
+    s.launch(s.hi, 2)
+    s.spin(2, .5)
+    s.diveStop(s.low, 3)
+    s.spin(4, .5, 3)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
+    s.fly(s.lf1, s.mid, 0, .5, 2.5)
+    s.spin('-=180_ccw', 0, .7)
+    s.path(mp1, 3)
+    s.innerSpin('-135_ccw', 0, 2.5, '<+=1.1')
+    s.innerSpin(3, .5, 2.5)
+    s.fly(s.rt1, s.low)
+    s.innerSpin('-=360_ccw', .5, 2.5)
+    s.fly(centre, s.low, 0, .5, 1.5)
+    s.innerSpin('-=450_ccw', .5, .8)
+    s.land()
 
     s.doTimeline();
 }
@@ -340,10 +343,10 @@ function L8() {
 
     let invSlide1 = {
         path: [
-            {x: rtIsh - 40 , y: lowIsh},
-            {x: 0, y: low},
-            {x: lfIsh + 40 , y: lowIsh},
-            {x: lfIsh, y: mid},
+            {x: s.rt1 - 40 , y: s.lowIsh},
+            {x: 0, y: s.low},
+            {x: s.lf1 + 40 , y: s.lowIsh},
+            {x: s.lf1, y: s.mid},
         ],
         start: 0,
         end: 1,
@@ -353,11 +356,11 @@ function L8() {
 
     let invSlide2 = {
         path: [
-            {x: lfIsh, y: mid},
-            {x: lfIsh + 40 , y: lowIsh},
-            {x: 0, y: low},
-            {x: rtIsh - 40 , y: lowIsh},
-            {x: rtIsh, y: lowIsh},
+            {x: s.lf1, y: s.mid},
+            {x: s.lf1 + 40 , y: s.lowIsh},
+            {x: 0, y: s.low},
+            {x: s.rt1 - 40 , y: s.lowIsh},
+            {x: s.rt1, y: s.lowIsh},
         ],
         start: 0,
         end: 1,
@@ -366,91 +369,91 @@ function L8() {
     }
 
     let s = new Stepper()
-        .setStart(k, kImg)
-        .launch(hiIsh, 2)
+    s.setStart(k, kImg)
+    s.launch(s.hiIsh, 2)
     // North
-        .fly(-aBit, hiIsh)
-        .fly(centre, hiIsh)
-        .fly(centre, hi)
-        .fly(centre, hiIsh)
-        .spin('-=45_ccw', .5, .3)
-        .spin('+=90_cw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
-        .spin(2, .5)
+    s.fly(-aBit, s.hiIsh)
+    s.fly(centre, s.hiIsh)
+    s.fly(centre, s.hi)
+    s.fly(centre, s.hiIsh)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('+=90_cw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin(2, .5)
     // South
-        .diveStop(low)
-        .fly(-aBit, low)
-        .fly(centre, low)
-        .fly(centre, lowIsh)
-        .fly(centre, low)
-        .spin('-=45_ccw', .5, .3)
-        .spin('+=90_cw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
+    s.diveStop(s.low)
+    s.fly(-aBit, s.low)
+    s.fly(centre, s.low)
+    s.fly(centre, s.lowIsh)
+    s.fly(centre, s.low)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('+=90_cw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
     // East
-        .fly(rtIsh, mid, 3, .5, 1.8)
-        .fly(rtIsh, hiIsh)
-        .fly(rtIsh, mid)
-        .fly(rtIsh + aBit, mid)
-        .fly(rtIsh, mid)
-        .spin('-=45_ccw', .5, .3)
-        .spin('+=90_cw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
+    s.fly(s.rt1, s.mid, 3, .5, 1.8)
+    s.fly(s.rt1, s.hiIsh)
+    s.fly(s.rt1, s.mid)
+    s.fly(s.rt1 + aBit, s.mid)
+    s.fly(s.rt1, s.mid)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('+=90_cw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
     // West
-        .path(invSlide1, 3)
-        .innerSpin(2, 0, 3, '<')
-        .fly(lfIsh, hiIsh)
-        .fly(lfIsh, mid)
-        .fly(lfIsh - aBit, mid)
-        .fly(lfIsh, mid)
-        .spin('-=45_ccw', .5, .3)
-        .spin('+=90_cw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
+    s.path(invSlide1, 3)
+    s.innerSpin(2, 0, 3, '<')
+    s.fly(s.lf1, s.hiIsh)
+    s.fly(s.lf1, s.mid)
+    s.fly(s.lf1 - aBit, s.mid)
+    s.fly(s.lf1, s.mid)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('+=90_cw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
     // North East
-        .fly(rtIsh, hiIsh, '+=135', .5, 1.5)
-        .fly(rtIsh - aBit, hi)
-        .fly(rtIsh, hiIsh)
-        .fly(rtIsh + aBit, hiIsh - aBit)
-        .fly(rtIsh, hiIsh)
-        .spin('-=45_ccw', .5, .3)
-        .spin('+=90_cw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
+    s.fly(s.rt1, s.hiIsh, '+=135', .5, 1.5)
+    s.fly(s.rt1 - aBit, s.hi)
+    s.fly(s.rt1, s.hiIsh)
+    s.fly(s.rt1 + aBit, s.hiIsh - aBit)
+    s.fly(s.rt1, s.hiIsh)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('+=90_cw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
     // South West
-        .fly(lfIsh, lowIsh, -2, .5, 1.5)
-        .fly(lfIsh - aBit, lowIsh - aBit)
-        .fly(lfIsh, lowIsh)
-        .fly(lfIsh - aBit, lowIsh + aBit)
-        .fly(lfIsh, lowIsh)
-        .spin('-=45_ccw', .5, .3)
-        .spin('+=90_cw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
+    s.fly(s.lf1, s.lowIsh, -2, .5, 1.5)
+    s.fly(s.lf1 - aBit, s.lowIsh - aBit)
+    s.fly(s.lf1, s.lowIsh)
+    s.fly(s.lf1 - aBit, s.lowIsh + aBit)
+    s.fly(s.lf1, s.lowIsh)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('+=90_cw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
     // North West
-        .fly(lfIsh - 30, lowIsh - 35, 2)
-        .fly(lfIsh, hiIsh, -1)
-        .fly(lfIsh + aBit, hiIsh - aBit)
-        .fly(lfIsh, hiIsh)
-        .fly(lfIsh - aBit, hiIsh - aBit)
-        .fly(lfIsh, hiIsh)
-        .spin('-=45_ccw', .5, .3)
-        .spin('+=90_cw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
+    s.fly(s.lf1 - 30, s.lowIsh - 35, 2)
+    s.fly(s.lf1, s.hiIsh, -1)
+    s.fly(s.lf1 + aBit, s.hiIsh - aBit)
+    s.fly(s.lf1, s.hiIsh)
+    s.fly(s.lf1 - aBit, s.hiIsh - aBit)
+    s.fly(s.lf1, s.hiIsh)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('+=90_cw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
     // South East
-        .path(invSlide2, 3)
-        .innerSpin(-2, 0, 3, '<')
-        .fly(rtIsh + aBit, lowIsh - aBit)
-        .fly(rtIsh, lowIsh)
-        .fly(rtIsh + aBit, lowIsh + aBit)
-        .fly(rtIsh, lowIsh)
-        .spin('-=45_ccw', .5, .3)
-        .spin('+=90_cw', .5, .3)
-        .spin('-=45_ccw', .5, .3)
+    s.path(invSlide2, 3)
+    s.innerSpin(-2, 0, 3, '<')
+    s.fly(s.rt1 + aBit, s.lowIsh - aBit)
+    s.fly(s.rt1, s.lowIsh)
+    s.fly(s.rt1 + aBit, s.lowIsh + aBit)
+    s.fly(s.rt1, s.lowIsh)
+    s.spin('-=45_ccw', .5, .3)
+    s.spin('+=90_cw', .5, .3)
+    s.spin('-=45_ccw', .5, .3)
     // Finish
-        .fly(centre, hi, '+=225_cw', .5, 2)
-        .spin(-2, .5)
-        .diveStop(mid, 1)
-        .spin(4, .5)
-        .diveStop(low)
-        .spin(-2, .5)
-        .land()
+    s.fly(centre, s.hi, '+=225_cw', .5, 2)
+    s.spin(-2, .5)
+    s.diveStop(s.mid, 1)
+    s.spin(4, .5)
+    s.diveStop(s.low)
+    s.spin(-2, .5)
+    s.land()
 
     s.doTimeline();
 }
