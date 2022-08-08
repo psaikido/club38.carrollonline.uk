46d73b20886df39cda2561c42c65f7b82d0aceb2
Hugh Carroll <hughie@carrollonline.co.uk>
2022-08-08 14:23:44 +0100

    Fine tune M14.

--- assets/js/patterns/dan.js
+++ assets/js/patterns/dan.js
@@ -317,22 +317,31 @@ function M13() {
 }
 
 function M14() {
-    reset();
+    reset()
+    let s = new Stepper()
 
-    let s = new Stepper();
-    s.setStart(k, kImg);
+    let svg = document.getElementById('club38-svg');
+    let c3Text = '<circle id="circle3" cx="320" cy="210" r="80" fill="none" />';
+    let c3 = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    c3.setAttribute('id', 'circle3');
+    c3.setAttribute('cx', '400');
+    c3.setAttribute('cy', '210');
+    c3.setAttribute('r', '120');
+    c3.setAttribute('fill', 'none');
+    svg.appendChild(c3);
+
+    MotionPathPlugin.convertToPath('#circle3');
 
     let slideCircle = {
-        path: '#slideCircle',
-        align: '#slideCircle',
-        autoRotate: true,
-        alignOrigin: [0.5, 0.35],
-        start: 0,
-        end: 1,
-        offsetX: 3,
-        offsetY: 27,
+        path: '#circle3',
+        align: '#circle3',
+        autoRotate: -90,
+        alignOrigin: [.5, .5],
+        start: .25,
+        end: 1.25,
+        offsetX: -2,
+        offsetY: -20,
     }
-
     // #3 walk-it-off
     s.spin('+=30_cw', .3, .5);
     s.spin('-=60_ccw', .3, .5);
@@ -344,56 +353,57 @@ function M14() {
     s.ptn = [];
 
     // #4 launch-snaps
-    s.launch(low, .5);
+    s.launch(s.low, .5);
     s.spin(3, .5, .5);
-    s.fly(lfIsh, low, 0, .5, 1);
+    s.fly(s.lf1, s.low, 0, .5, 1);
     s.spin(-7, .5, 1);
     let launchSnaps = s.ptn;
     s.ptn = [];
 
     // #8 don-fox-snap-box 
-    s.fly(lfIsh, hiIsh);
+    s.fly(s.lf1, s.hiIsh);
     s.innerSpin(1, .9, .1, '<');
-    s.fly(rtIsh, hiIsh);
+    s.fly(s.rt1, s.hiIsh);
     s.innerSpin(1, .9, .1, '<');
-    s.fly(rtIsh, lowIsh);
+    s.fly(s.rt1, s.low);
     s.innerSpin(1, .9, .1, '<');
-    s.fly(lfIsh, lowIsh, 0, .5, 1.3);
+    s.fly(s.lf1, s.low);
+    s.innerSpin(1, .9, .1, '<');
     let snapBox = s.ptn;
     s.ptn = [];
 
     // zig zag up
-    s.spin('-=225_ccw', .5, 2);
-    s.fly(lfIsh + 100, lowIsh - 100, 0, .5, .9);
+    s.spin('-=315_ccw', .5, 2);
+    s.fly(s.centre, s.mid, 0, .5, .9);
     s.spin(3, .5, .7);
-    s.fly(lfIsh, hiIsh + 30, 0, .5, .9);
+    s.fly(s.lf1, s.hiIsh, 0, .5, .9);
     s.innerSpin(-3, .5, .7);
-    s.fly(centre, hi, 0, .5, .9);
+    s.fly(s.centre, s.hi, 0, .5, .9);
     s.spin('-=225_ccw', .5, .7);
     let zigZagUp = s.ptn;
     s.ptn = [];
 
     // #10 shock-circle-snap 
-    s.diveStop(low);
-    s.path(slideCircle, 6, 4);
+    s.diveStop(s.low);
+    s.path(slideCircle, 0, .5, 6);
     s.spin(-5, .1, .7);
     let shockCircleSnap = s.ptn;
     s.ptn = [];
 
     // tip turns
-    s.fly(centre, low - 45);
+    s.fly(s.centre, s.low - 45);
     s.spin(-2, 0, 1, '<');
-    s.fly(centre, low - 100);
+    s.fly(s.centre, s.low - 100);
     s.spin(2, 0, 1, '<');
-    s.fly(centre, mid);
+    s.fly(s.centre, s.mid);
     s.spin(-2, 0, 1, '<');
     let tipTurns = s.ptn;
     s.ptn = [];
 
     // #9 snap-clock-snap
-    s.fly(lfIsh, mid);
+    s.fly(s.lf1, s.mid);
     s.spin(6, .5, 1);
-    s.fly(centre, mid);
+    s.fly(s.centre, s.mid);
     s.innerSpin(-4, .5, .7);
     s.spin(1, .5, .3);
     s.spin(1, .5, .3);
@@ -404,43 +414,43 @@ function M14() {
     s.ptn = [];
 
     // #6 go-snap-back-vslide
-    s.fly(rtIsh, mid);
+    s.fly(s.rt1, s.mid);
     s.spin(4, 0, .7);
-    s.fly(centre, mid, 0, .5, 1.2);
+    s.fly(s.centre, s.mid, 0, .5, 1.2);
     s.spin(-3, 0, .5);
-    s.fly(rtIsh, mid, 0, 0, 1.5);
+    s.fly(s.rt1, s.mid, 0, 0, 1.5);
     let goSnapBackV = s.ptn;
     s.ptn = [];
 
     // #7 v-slide-v
-    s.fly(rtIsh, low);
+    s.fly(s.rt1, s.low);
     s.spin(4, 0, .7);
-    s.fly(lfIsh, low, 0, 0, 3);
+    s.fly(s.lf1, s.low, 0, 0, 3);
     s.spin(4, 1.2, .7, '<');
     let vSlideV = s.ptn;
     s.ptn = [];
 
     // tumbleweed up
-    s.fly(centre, hi, -4, .5, 6);
+    s.fly(s.centre, s.hi, -4, .5, 6);
     let tumbleweedUp = s.ptn;
     s.ptn = [];
 
     // #1/#2 shock-snap-tip
-    s.diveStop(low, 1);
+    s.diveStop(s.low, 1);
     s.spin(5, .2, .8);
     s.land();
-    s.fly(centre, low, 0, .7);
-    s.fly(centre + 60, low, -6, .5, 1.5)
-    s.land(1, .5);
-    s.fly(centre + 60, low, 0, 1, 1);
-    s.fly(centre, low, 3, .5, .8);
+    s.fly(s.centre, s.low, 0, .7);
+    s.fly(s.centre + 60, s.low, -6, .5, 1.5)
+    s.land();
+    s.fly(s.centre + 60, s.low, 0, 1, 1);
+    s.fly(s.centre, s.low, 3, .5, .8);
     let shockSnap = s.ptn;
     s.ptn = [];
 
     // #5 shock-clock-snap
-    s.fly(centre, hi, 0, .5, 2);
+    s.fly(s.centre, s.hi, 0, .5, 2);
     s.spin(-2, .5, .5);
-    s.diveStop(low, .8);
+    s.diveStop(s.low, .8);
     s.spin('-=45_ccw', .5, .3);
     s.spin('-=45_ccw', .5, .3);
     s.spin('-=45_ccw', .5, .3);
