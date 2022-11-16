72aa471de78a793dbd30f566974800905f56685c
Hugh Carroll <hughie@carrollonline.co.uk>
2022-11-16 22:27:03 +0000

    Add presets for 'blues' and 'greenblack'.

--- assets/js/kite-color.js
+++ assets/js/kite-color.js
@@ -62,6 +62,8 @@ $( document ).ready(function() {
     $('#presets').change(function() {
         let scheme = $(this).val();
         if (scheme == 'blackred') blackred();
+        if (scheme == 'blues') blues();
+        if (scheme == 'greenblack') greenblack();
     });
 
     /* Called by the user choosing a color and a panel and also by
@@ -81,24 +83,70 @@ $( document ).ready(function() {
 
     function blackred() {
         changeClr($('#L1'), 'black');
-        changeClr($('#L2'), 'red');
-        changeClr($('#L3'), 'light-grey');
-        changeClr($('#L4'), 'black');
-        changeClr($('#L5'), 'white');
-        changeClr($('#L6'), 'red');
-        changeClr($('#L7'), 'black');
-        changeClr($('#L8'), 'light-grey');
-        changeClr($('#L9'), 'red');
-        changeClr($('#L10'), 'light-grey');
         changeClr($('#R1'), 'black');
+        changeClr($('#L2'), 'red');
         changeClr($('#R2'), 'red');
+        changeClr($('#L3'), 'light-grey');
         changeClr($('#R3'), 'light-grey');
+        changeClr($('#L4'), 'black');
         changeClr($('#R4'), 'black');
+        changeClr($('#L5'), 'white');
         changeClr($('#R5'), 'white');
+        changeClr($('#L6'), 'red');
         changeClr($('#R6'), 'red');
+        changeClr($('#L7'), 'black');
         changeClr($('#R7'), 'black');
+        changeClr($('#L8'), 'light-grey');
         changeClr($('#R8'), 'light-grey');
+        changeClr($('#L9'), 'red');
         changeClr($('#R9'), 'red');
+        changeClr($('#L10'), 'light-grey');
         changeClr($('#R10'), 'light-grey');
+    }
+
+    function blues() {
+        changeClr($('#L1'), 'light-grey');
+        changeClr($('#R1'), 'light-grey');
+        changeClr($('#L2'), 'white');
+        changeClr($('#R2'), 'white');
+        changeClr($('#L3'), 'dark-blue');
+        changeClr($('#R3'), 'dark-blue');
+        changeClr($('#L4'), 'bright-blue');
+        changeClr($('#R4'), 'bright-blue');
+        changeClr($('#L5'), 'light-grey');
+        changeClr($('#R5'), 'light-grey');
+        changeClr($('#L6'), 'bright-blue');
+        changeClr($('#R6'), 'bright-blue');
+        changeClr($('#L7'), 'light-grey');
+        changeClr($('#R7'), 'light-grey');
+        changeClr($('#L8'), 'dark-blue');
+        changeClr($('#R8'), 'dark-blue');
+        changeClr($('#L9'), 'dark-grey');
+        changeClr($('#R9'), 'dark-grey');
+        changeClr($('#L10'), 'bright-blue');
+        changeClr($('#R10'), 'bright-blue');
+    }
+
+    function greenblack() {
+        changeClr($('#L1'), 'green');
+        changeClr($('#L2'), 'green');
+        changeClr($('#L3'), 'black');
+        changeClr($('#L4'), 'black');
+        changeClr($('#L5'), 'green');
+        changeClr($('#L6'), 'black');
+        changeClr($('#L7'), 'green');
+        changeClr($('#L8'), 'black');
+        changeClr($('#L9'), 'green');
+        changeClr($('#L10'), 'green');
+        changeClr($('#R1'), 'black');
+        changeClr($('#R2'), 'black');
+        changeClr($('#R3'), 'green');
+        changeClr($('#R4'), 'green');
+        changeClr($('#R5'), 'black');
+        changeClr($('#R6'), 'green');
+        changeClr($('#R7'), 'black');
+        changeClr($('#R8'), 'green');
+        changeClr($('#R9'), 'black');
+        changeClr($('#R10'), 'black');
     }
 });
