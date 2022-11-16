3fdf3230100a8eaba2f8619bb85ce86a73b0aabf
Hugh Carroll <hughie@carrollonline.co.uk>
2022-11-16 20:13:20 +0000

    A central panel reversal widget at Lolly's request.

--- assets/js/kite-color.js
+++ assets/js/kite-color.js
@@ -21,20 +21,47 @@ $( document ).ready(function() {
         $('svg.revolution-reflex text').toggle();
     });
 
+    /* Take out clutter for a clean screenshot */
     $('button#ready-for-screenshot').click(function() {
         screenshotToggle();
     });
 
+    /* Used to get back to the full editing mode */
     $('button#show-toolbox').click(function() {
         screenshotToggle();
     });
 
+    /* Take all color choices off */
     $('button#reset').click(function() {
         $('.revolution-reflex').children('.panel').each(function(i) {
             $(this).removeClass();
             $(this).addClass('panel');
         });
     });
+
+    $('.toolbox #reverse-center').click(function() {
+        let centerPanel = $('svg.revolution-reflex #center-panel');
+        let logo = $('svg.revolution-reflex .logo');
+
+        if (centerPanel.attr("class") == 'center') {
+            centerPanel.removeClass();
+            centerPanel.addClass('center reverse');
+            logo.children().each(function() {
+                $(this).removeClass();
+                $(this).addClass('logo reverse');
+            });
+        } else {
+            centerPanel.removeClass();
+            centerPanel.addClass('center');
+            logo.children().each(function() {
+                $(this).removeClass();
+                $(this).addClass('logo');
+            });
+        }
+    });
+
+    // Toggle between editing and screenshot modes
+    // Called from both modes by different buttons
 
     function screenshotToggle() {
         $('.kite-colorizer p').toggle();
