a1c00798fcfb82063bcd35fddf7637d6d2fde5a9
Hugh Carroll <hughie@carrollonline.co.uk>
2022-11-18 20:16:54 +0000

    There is a smaller range of presets for the Tarantula as opposed to the XX.

--- assets/js/presets-tarantula.js
+++ assets/js/presets-tarantula.js
@@ -1,34 +1,10 @@
 /* Preset color schemes */
 $('#presets').change(function() {
     let scheme = $(this).val();
-    if (scheme == 'blackred') blackred();
     if (scheme == 'blues') blues();
-    if (scheme == 'greenblack') greenblack();
     if (scheme == 'hot') hot();
     if (scheme == 'purplelimeblack') purplelimeblack();
-    if (scheme == 'redwhiteblue') redwhiteblue();
 });
-
-function blackred() {
-    changeClr($('#L1'), 'black');
-    changeClr($('#R1'), 'black');
-    changeClr($('#L2'), 'red');
-    changeClr($('#R2'), 'red');
-    changeClr($('#L3'), 'light-grey');
-    changeClr($('#R3'), 'light-grey');
-    changeClr($('#L4'), 'black');
-    changeClr($('#R4'), 'black');
-    changeClr($('#L5'), 'white');
-    changeClr($('#R5'), 'white');
-    changeClr($('#L6'), 'red');
-    changeClr($('#R6'), 'red');
-    changeClr($('#L7'), 'black');
-    changeClr($('#R7'), 'black');
-    changeClr($('#L8'), 'light-grey');
-    changeClr($('#R8'), 'light-grey');
-    changeClr($('#L10'), 'light-grey');
-    changeClr($('#R10'), 'light-grey');
-}
 
 function blues() {
     changeClr($('#L1'), 'light-grey');
@@ -49,27 +25,6 @@ function blues() {
     changeClr($('#R8'), 'dark-blue');
     changeClr($('#L10'), 'bright-blue');
     changeClr($('#R10'), 'bright-blue');
-}
-
-function greenblack() {
-    changeClr($('#L1'), 'green');
-    changeClr($('#L2'), 'green');
-    changeClr($('#L3'), 'black');
-    changeClr($('#L4'), 'black');
-    changeClr($('#L5'), 'green');
-    changeClr($('#L6'), 'black');
-    changeClr($('#L7'), 'green');
-    changeClr($('#L8'), 'black');
-    changeClr($('#L10'), 'green');
-    changeClr($('#R1'), 'black');
-    changeClr($('#R2'), 'black');
-    changeClr($('#R3'), 'green');
-    changeClr($('#R4'), 'green');
-    changeClr($('#R5'), 'black');
-    changeClr($('#R6'), 'green');
-    changeClr($('#R7'), 'black');
-    changeClr($('#R8'), 'green');
-    changeClr($('#R10'), 'black');
 }
 
 function hot() {
@@ -96,41 +51,20 @@ function hot() {
 function purplelimeblack() {
     changeClr($('#L1'), 'purple');
     changeClr($('#R1'), 'purple');
-    changeClr($('#L2'), 'black');
-    changeClr($('#R2'), 'black');
+    changeClr($('#L2'), 'dark-grey');
+    changeClr($('#R2'), 'dark-grey');
     changeClr($('#L3'), 'flo-yellow');
     changeClr($('#R3'), 'flo-yellow');
     changeClr($('#L4'), 'purple');
     changeClr($('#R4'), 'purple');
-    changeClr($('#L5'), 'black');
-    changeClr($('#R5'), 'black');
+    changeClr($('#L5'), 'dark-grey');
+    changeClr($('#R5'), 'dark-grey');
     changeClr($('#L6'), 'flo-yellow');
     changeClr($('#R6'), 'flo-yellow');
     changeClr($('#L7'), 'flo-yellow');
     changeClr($('#R7'), 'flo-yellow');
-    changeClr($('#L8'), 'black');
-    changeClr($('#R8'), 'black');
+    changeClr($('#L8'), 'dark-grey');
+    changeClr($('#R8'), 'dark-grey');
     changeClr($('#L10'), 'purple');
     changeClr($('#R10'), 'purple');
-}
-
-function redwhiteblue() {
-    changeClr($('#L1'), 'bright-blue');
-    changeClr($('#R1'), 'bright-blue');
-    changeClr($('#L2'), 'white');
-    changeClr($('#R2'), 'white');
-    changeClr($('#L3'), 'red');
-    changeClr($('#R3'), 'red');
-    changeClr($('#L4'), 'red');
-    changeClr($('#R4'), 'red');
-    changeClr($('#L5'), 'dark-blue');
-    changeClr($('#R5'), 'dark-blue');
-    changeClr($('#L6'), 'dark-blue');
-    changeClr($('#R6'), 'dark-blue');
-    changeClr($('#L7'), 'white');
-    changeClr($('#R7'), 'white');
-    changeClr($('#L8'), 'dark-blue');
-    changeClr($('#R8'), 'dark-blue');
-    changeClr($('#L10'), 'red');
-    changeClr($('#R10'), 'red');
 }
